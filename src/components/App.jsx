00ca6d577ecdcf66835c3f444afb1d7443f404bd
Gaston Noya <nvfede@gmail.com>
2020-10-18 14:52:10 +0200

    added Layout component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import { render } from "@testing-library/react";
 import React, { Fragment } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Menu from "./Menu";
 
 import Home from "../Pages/Home";
 import Country from "../Pages/Country";
@@ -9,17 +8,19 @@ import About from "../Pages/About";
 
 import "../assets/styles/main.scss";
 import NotFound from "./NotFound";
+import Layout from "./Layout";
 
 const App = () => (
   <Fragment>
     <BrowserRouter>
-      <Menu />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/countries/:slug" component={Country} />
-        <Route component={NotFound} />
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/countries/:slug" component={Country} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
     </BrowserRouter>
   </Fragment>
 );
---
+++ src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Menu from './Menu';
+
+const Layout = ({children}) => (
+
+    <div className="App">
+        <Menu />
+        {children}
+    </div>
+    
+
+)
+export default Layout
